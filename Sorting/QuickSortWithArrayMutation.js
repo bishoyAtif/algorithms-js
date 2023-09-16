6091c28e193f8d2a1cdb042fe1104a9359058fd9
Bishoy Atif <bishooy.atif@gmail.com>
2023-09-16 22:36:24 +0300

    Fix QuickSortWithArrayMutation

--- Sorting/QuickSortWithArrayMutation.js
+++ Sorting/QuickSortWithArrayMutation.js
@@ -1,11 +1,18 @@
-function quickSort(arr) {
-    if (arr.length < 2) {
+function quickSort(arr, l, r) {
+    if (l >= r) {
         return arr
     }
 
-    let midIndex = Math.floor(arr.length / 2)
+    const newPivot = partitionArray(arr, l, r)
+    quickSort(arr, l, newPivot - 1)
+    quickSort(arr, newPivot, r)
+
+    return arr
+}
+
+function partitionArray(arr, l, r) {
+    let midIndex = Math.floor((l + r) / 2)
     let pivot = arr[midIndex]
-    let l = 0, r = arr.length - 1
 
     while (l <= r) {
         while (arr[l] < pivot) {
@@ -15,7 +22,7 @@ function quickSort(arr) {
             r--
         }
 
-        if (l < r) {
+        if (l <= r) {
             const tmp = arr[l]
             arr[l] = arr[r]
             arr[r] = tmp
@@ -24,9 +31,8 @@ function quickSort(arr) {
         }
     }
 
-    console.log(arr.slice(0, l), arr[l], arr.slice(l + 1), '\n')
-    return [...quickSort(arr.slice(0, l)), arr[l], ...quickSort(arr.slice(l + 1))]
+    return l
 }
 
-const unsortedArray = [15, 2, -1, 16, 6, 7, 20, 1, 4, 3, 11]
-console.log(quickSort(unsortedArray))
+const unsortedArray = [2, 16, 6, 7, 20, 11]
+console.log(quickSort(unsortedArray, 0, unsortedArray.length - 1))
